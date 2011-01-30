01f9c587312d07e8f22a5d42a4cb27737689a6ef
Bryan Larson <goblom10@yahoo.com>
2011-01-30 14:32:58 -0800

    Preparing for upload.

---
+++ ooPo-prebuilt-ps3toolchain (Linux)/Setup {usr.user.desktop.ps3dev}.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# Thanks to ooPo for making this open source toolchain and to the PSL1GHT Team
+# Pre-Compiled Toolchain Made By Goblom - psgroove.com
+# Downloaded from https://gihub.com/Goblom/allps3tools.git
+# Soon to me moved to https://github.com/Goblom/ooPo-Pre.Comp.ps3toolchain(Linux)
+# Run this every time you start the Terminal
+# Change {User} to the username that you are on
+
+## Set up the PS3DEV environment.
+export PS3DEV=/home/{User}/Desktop/ps3dev
+export PATH=$PATH:$PS3DEV/bin
+export PATH=$PATH:$PS3DEV/host/ppu/bin
+export PATH=$PATH:$PS3DEV/host/spu/bin
+
+## Set up the PSL1GHT environment.
+export PSL1GHT=$PS3DEV/psl1ght
+export PATH=$PATH:$PSL1GHT/host/bin
